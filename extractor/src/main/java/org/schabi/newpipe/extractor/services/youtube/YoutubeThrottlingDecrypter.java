2d8853d1beb445b97d77746c067872dfc1f42eb1
Fabiano <fabianoa7x2014@gmail.com>
2022-09-15 17:31:17 -0300

    update2

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/YoutubeThrottlingDecrypter.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/YoutubeThrottlingDecrypter.java
@@ -36,8 +36,8 @@ import javax.annotation.Nonnull;
  */
 public final class YoutubeThrottlingDecrypter {
 
-    public static final Pattern N_PARAM_PATTERN = Pattern.compile("[&?]n=([^&]+)");
-    public static final Pattern DECRYPT_FUNCTION_NAME_PATTERN = Pattern.compile(
+    public static Pattern N_PARAM_PATTERN = Pattern.compile("[&?]n=([^&]+)");
+    public static Pattern DECRYPT_FUNCTION_NAME_PATTERN = Pattern.compile(
             "\\.get\\(\"n\"\\)\\)&&\\(b=([a-zA-Z0-9$]+)(?:\\[(\\d+)])?\\([a-zA-Z0-9]\\)");
 
     private static final Map<String, String> N_PARAMS_CACHE = new HashMap<>();
